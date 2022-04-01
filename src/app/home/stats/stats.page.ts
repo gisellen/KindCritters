ae67b78bb1a2eaceac1f44861a0dea9b2abb3758
Giselle Domingo <giselle.domingo.x@gmail.com>
2022-04-01 10:13:33 -0400

    Added Chart.js to implement statistics

--- src/app/home/stats/stats.page.ts
+++ src/app/home/stats/stats.page.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { registerables } from 'chart.js';
+import Chart from 'chart.js/auto';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.page.html',
   styleUrls: ['./stats.page.scss'],
 })
-export class StatsPage implements OnInit {
-
+export class StatsPage implements AfterViewInit {
+  @ViewChild('barCanvas') private barCanvas: ElementRef;
+  barChart: any;
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.barChartMethod();
   }
 
+  barChartMethod(){
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
+        datasets: [{
+          label: '# of votes',
+          data: [200, 50, 30, 15, 20, 34],
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderRadius: 10,
+          borderColor: [
+            'rgba(0,0,0,0)',
+            'rgba(0,0,0,0)',
+            'rgba(0,0,0,0)',
+            'rgba(0,0,0,0)',
+            'rgba(0,0,0,0)',
+            'rgba(0,0,0,0)'
+          ],
+          borderWidth: 1
+        }]
+      }
+    })
+  }
 }
