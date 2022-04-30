e37da7ab7edd4ef10d9fc381d3a7b7ef5e0ecb34
richardljohn <richardljohn@gmail.com>
2022-04-30 17:29:42 -0400

    Auth Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,27 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
+
   {
-    path: '',
-    redirectTo: 'home/tabs/reminders',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home/tabs',
-    redirectTo: 'home/tabs/reminders',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '', redirectTo: 'home/tabs', pathMatch: 'full'
   },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'reminders',
+    loadChildren: () => import('./home/reminders/reminders.module').then(m => m.RemindersPageModule),
+    canLoad: [AuthGuard]
+  },
+
 ];
 
 @NgModule({
