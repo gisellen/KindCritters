5585fac78b377009cf9e2eb06119345398930a9e
Gary Feng <garyfeng961@gmail.com>
2022-05-02 18:38:40 -0400

    fix code

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,53 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
-
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
   styleUrls: ['./auth.page.scss'],
 })
-
-
 export class AuthPage implements OnInit {
-
   isLoading = false;
   isLogin = true;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private loadingCtrl: LoadingController
+  ) {}
 
-  ngOnInit() {
-  }
-  onLogin(){
+  ngOnInit() {}
+  onLogin() {
     this.isLoading = true;
     this.authService.login();
-    setTimeout( ()=> { 
-      this.isLoading = false;
-      this.router.navigateByUrl('/home/tabs/reminders')
-    }, 1500);
+    this.loadingCtrl
+      .create({
+        keyboardClose: true,
+        message: 'Loading Please Wait...',
+        spinner: 'crescent',
+      })
+      .then((loadingEl) => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl('/home/tabs/reminders');
+        }, 1500);
+      });
   }
 
-  onSwitchAuthMode(){ 
+  onSwitchAuthMode() {
     this.isLogin = !this.isLogin;
   }
 
   onSubmit(form: NgForm) {
-    if(!form.valid){
+    if (!form.valid) {
       return;
     }
 
     const email = form.value.email;
     const password = form.value.password;
     console.log(email, password);
-    if(this.isLogin){
-
+    if (this.isLogin) {
+      // send a request to login server
+    } else {
+      // send a request to signup server
     }
-    else { 
-
-    }
-
   }
-
 }
