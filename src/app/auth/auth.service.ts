2d4c154ca90e04aaf18d662edfd6c3e1d07ce520
Gisellen <giselle.domingo.x@gmail.com>
2022-05-09 18:02:24 -0400

    Fixed auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AuthService {
-  private _userIsAutheticated = true;
+  private _userIsAutheticated = false;
 
   get userIsAutheticated() {
     return this._userIsAutheticated;
